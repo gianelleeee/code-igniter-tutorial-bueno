24998f7775ea5ca731a0966637015796097067e3
gianelleeee <gianelleeee@gmail.com>
2023-03-31 14:32:24 +0800

    first commit

---
+++ application/controllers/Pages.php
@@ -0,0 +1,23 @@
+<?php
+
+class Pages extends CI_controller{
+
+    public function view(){
+
+        $page = "home";
+
+        if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
+            show_404();
+        }
+
+        $data['title'] = "Tutorial";
+
+        $this->load->view('templates/header');
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer');
+    }
+
+
+
+
+}
